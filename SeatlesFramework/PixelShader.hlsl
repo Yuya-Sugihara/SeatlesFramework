2079b4bbf7f302422c9fb78b61063efae3f9103c
Yuya-Sugihara <yuuyamix3@gmail.com>
2021-04-05 22:42:40 +0900

    シェーダーで使用するOutput構造体を作成、シェーダーファイルの編集

--- SeatlesFramework/PixelShader.hlsl
+++ SeatlesFramework/PixelShader.hlsl
@@ -1,12 +1,6 @@
+#include "BasicType.hlsli"
 
-struct Input
+float4 pixelShader(Output input) : SV_TARGET
 {
-	float4 pos:POSITION;
-	float4 svpos:SV_POSITION;
-};
-
-float4 pixelShader(Input input) : SV_TARGET
-{
-	//return float4(1.0f, 1.0f, 1.0f, 1.0f);
-	return float4((float2(1,1) + input.pos.xy * 0.5f),1,1);
+	return float4(input.uv,1,1);
 }
--- SeatlesFramework/VertexShader.hlsl
+++ SeatlesFramework/VertexShader.hlsl
@@ -1,15 +1,10 @@
+#include "BasicType.hlsli"
 
-struct Output
+Output vertexShader(float4 pos : POSITION,float2 uv:TEXCOORD)
 {
-	float4 pos:POSITION;
-	float4 svpos:SV_POSITION;
-};
-
-Output vertexShader(float4 pos : POSITION)
-{
-	//return pos;
 	Output output;
-	output.pos = pos;
 	output.svpos = pos;
+	output.uv = uv;
+
 	return output;
 }
